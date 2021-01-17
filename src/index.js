c1faa9b8bd0a68d1fd630a43526b4cde93f9bd7c
david-shakaya <devidoz666@gmail.com>
2021-01-17 19:32:31 +0200

    добавл метод Удаление из корзины

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ const count = (qwe) => {
 }
 
 
+
 // МАГАЗИН
 // - 1) - Создаем обьект items: [] в котором пустой масив, туда будем пушить(добавлять товар).
 const cart = {
@@ -83,16 +84,6 @@ const cart = {
   },
 };
 
-const listFruts = document.querySelector('.list-item-fruts');
-// const basket = document.querySelector('.basket-js');
-// const shoppingList = document.querySelector('.shopping-list');
-// const totalPrice = document.querySelector('.total-price-js');
-
-
-const arr = listFruts.children
-  
-
-
 // Дом узлы
 const ulRef = document.querySelector('.list-item-fruts');
 const ulQuRef = document.querySelector('.total-price-js');
@@ -102,148 +93,45 @@ const divName = document.querySelector('.name-js');
 const markup = store(storage)
 ulRef.insertAdjacentHTML('beforeend', markup)
 
-let previosId = 0
-
 ulRef.addEventListener('click', (e) => {
   if (e.target.nodeName !== 'BUTTON' ) {
     return
   }
-  const getIdCurrentElInDom = () => e.path[2].getAttribute('id')
-  // console.log(getIdCurrentElInDom());
-
+const getIdCurrentElInDom = () => e.path[2].getAttribute('id')
  const FindIdCurrentEl =()=> storage.find(el => {
    if (el.id === +getIdCurrentElInDom()) {
     return el
    }
  })
   
-  
-
   cart.abb(FindIdCurrentEl());
 
-          // cart.items.forEach(function (el) {
-          // console.log('ELEMENT TES' + el.name);
-          //   if (el.quantity > 0) {
-
-          //     const heading = document.createElement('p');
-          //     heading.textContent = `${el.name} - ${el.quantity}`;
-
-            
-          //     // divName.innerHTML += `<p>${el.name} - ${el.quantity}</p>`
-
-              
-          //   }
-  
-              //  const heading = document.createElement('p');
-              // heading.textContent = `${el.name} - ${el.quantity}`;
-
 const markup =cart.items.reduce(
-  (acc, el) => acc +  `<p>${el.name}-${el.quantity}</p>`,
+  (acc, el) => acc +  `<button class="btn-remove-elem" id="${el.name}">X</button><p>${el.name}-${el.quantity}</p>`,
   ''
   )
   divName.innerHTML = markup;
-
-
- 
-  if (FindIdCurrentEl().quantity > 0) {
-
-/*     const mar = cartTemplate(storage)
-    divName.innerHTML = mar */
+  ulQuRef.innerHTML =  `Общая сумма ${cart.totalPrice()} грн.`
+  // console.log('вы добаили' + FindIdCurrentEl().name);
 
 
 
-  /*    const quantityAfterAdd = document.querySelectorAll('.quantity');
-    const nameProductAfterAdd = document.querySelectorAll('.name-product')
-     quantityAfterAdd.forEach(el => {
-       if (+el.textContent === 0) {
-         el.classList.add('displ-none')
-       }
-       }) */
- 
-      //  nameProductAfterAdd.forEach(el => {
-      //        el.classList.add('displ-none')
-      //      })
-    // const mapArr = storage.find(el => {
-    //   if (el.quantity > 0) {
-    //     return el
-    //   }
-    // })
-
-
-
-    // console.log(mapArr);
-
-  }
-
-  
-  // else {
-       
-  //    const mar = cartTemplate(storage)
-  //   divName.innerHTML = mar
-  //      const quantityAfterAdd = document.querySelectorAll('.quantity');
-  //   const nameProductAfterAdd = document.querySelectorAll('.name-product')
-  //    quantityAfterAdd.forEach(el => {
-  //      if (+el.textContent === 0) {
-  //        el.classList.add('displ-none')
-  //      }
-  //    })
-    
-  //    nameProductAfterAdd.forEach(el => {
-  //            el.classList.add('displ-none')
-  //          })
-    // divName.innerHTML = `<p>количество ${FindIdCurrentEl().quantity}</p>`
-    //  divName.insertAdjacentHTML('beforeend', `<p>количество ${FindIdCurrentEl().quantity}</p>`)
-  // }
-
-  // const quantityAfterAdd = document.querySelectorAll('.quantity');
-  // const nameProductAfterAdd = document.querySelectorAll('.name-product')
-  //   quantityAfterAdd.forEach(el => {
-  //     if (+el.textContent === 0) {
-  //          el.classList.add('displ-none')
-  //          nameProductAfterAdd.forEach(el => {
-  //            el.classList.add('displ-none')
-  //          })
-  //        } else {
-  //         //  nameProductAfterAdd.classList.remove('displ-none')
-  //          nameProductAfterAdd.forEach(el => {
-  //            el.classList.remove('displ-none')
-  //          })
-  //     }
-  //   })
-
-
- 
-  // if (previosId === FindIdCurrentEl().id) {
-  //   divName.innerHTML = `<p>количество ${FindIdCurrentEl().quantity}</p>`
-   
-  //   if (e) {
+  const btnRemoveElem = document.querySelectorAll('.btn-remove-elem');
+  btnRemoveElem.forEach(el => {
+    console.log(el.attributes[1].nodeValue);
+    el.addEventListener('click', fnRemove)
+    function fnRemove() {
       
-  //      divName.insertAdjacentHTML ('beforeend',`
-  // <p>количество ${FindIdCurrentEl().quantity}</p>
-  // ` )
-  //   }
-  // }
-  // <h3>Общая сумма ${cart.totalPrice()} грн.</h3>
+      cart.remove(el.attributes[1].nodeValue)
+      // divName.innerHTML = markup;
+      ulQuRef.innerHTML =  `Общая сумма ${cart.totalPrice()} грн.`
 
-  console.log('previos'+previosId);
-  if (e) {
-    console.log( 'Предпосл'+cart.items.slice(-2)[0].id);
-    console.log('Текущ' + FindIdCurrentEl().id);
-    previosId =FindIdCurrentEl().id
-  }
-
-
- 
+    }
+  })
  
 
 
-  console.log('вы добаили' + FindIdCurrentEl().name);
 
-   cart.items.forEach(el => console.log(el))
-  console.log('Общая сумма' + cart.totalPrice());
-
-
-  
 })
 
 
